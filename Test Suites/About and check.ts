25575b8c50cccc18cd0bf9ebcd0bcb0cb271fdbd
C.Kamali <106811823+ckamali@users.noreply.github.com>
2024-08-11 13:49:32 +0530

    updated

---
+++ Test Suites/About and check.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>About and check</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>369303a5-3044-4db9-8f1c-658ea156b14d</testSuiteGuid>
+   <testCaseLink>
+      <guid>c10c88b4-bfba-4e32-9c96-c7a52686c3a0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/About</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
